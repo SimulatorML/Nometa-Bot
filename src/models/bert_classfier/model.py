69d527bc3500e97eb6366f264a9880dd8997a981
w3ban <w3bansae@gmail.com>
2023-05-03 12:52:08 +0300

    :shirt: refactor: add device to init variables in BertClassifier

--- src/models/bert_classfier/model.py
+++ src/models/bert_classfier/model.py
@@ -11,7 +11,8 @@ class BertClassifier():
     def __init__(self,
                  model_path: str = "cointegrated/rubert-tiny",
                  max_length: int = 32,
-                 padding: str = "max_length") -> None:
+                 padding: str = "max_length",
+                 device: str = "cpu") -> None:
 
         self.tokenizer = AutoTokenizer.from_pretrained(model_path, local_files_only=True)
         self.model = AutoModelForSequenceClassification.from_pretrained(
@@ -21,19 +22,20 @@ class BertClassifier():
             output_hidden_states=False)
         self.max_length = max_length
         self.padding = padding
+        self.device = device
 
-    def predict(self, text: str, device: str = "cpu"):
+    def predict(self, text: str):
 
         ids, mask = self._encode(text)
-        ids, mask = ids.to(device), mask.to(device)
+        ids, mask = ids.to(self.device), mask.to(self.device)
         with torch.no_grad():
             output = self.model(ids, token_type_ids=None, attention_mask=mask)
         prediction = np.argmax(output.logits.cpu().numpy()).flatten().item()
         return prediction
 
-    def predict_proba(self, text: str, device: str = "cpu"):
+    def predict_proba(self, text: str):
         ids, mask = self._encode(text)
-        ids, mask = ids.to(device), mask.to(device)
+        ids, mask = ids.to(self.device), mask.to(self.device)
         with torch.no_grad():
             output = self.model(ids, token_type_ids=None, attention_mask=mask)
         prediction = output.logits.cpu().numpy()[:, 1].item()
